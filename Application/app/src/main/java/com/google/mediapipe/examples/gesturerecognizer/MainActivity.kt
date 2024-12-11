23f18f5cad8c5a36839d2814b1ac71fb8855ad19
Vihaan Sapra <vihaansapra@gmail.com>
2024-12-11 12:01:08 +0530

    chores: removed gallery, nav code

--- Application/app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/MainActivity.kt
+++ Application/app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/MainActivity.kt
@@ -30,14 +30,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
-
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
-        val navController = navHostFragment.navController
-        activityMainBinding.navigation.setupWithNavController(navController)
-        activityMainBinding.navigation.setOnNavigationItemReselectedListener {
-            // ignore the reselection
-        }
     }
 
     override fun onBackPressed() {
